2bdc682c489901fe24275afe52b3b4c353bc6e3d
chiisen <chiisen@hotmail.com>
2025-09-22 14:36:59 +0800

    ✨ feat: 在設定版本編輯中新增更新時間欄位的日期時間輸入，並格式化輸出

--- my-admin/src/SettingVersion/SettingVersionEdit.jsx
+++ my-admin/src/SettingVersion/SettingVersionEdit.jsx
@@ -1,14 +1,33 @@
-import { Edit, SimpleForm, TextInput } from 'react-admin';
+import { Edit, SimpleForm, TextInput, DateTimeInput } from 'react-admin';
+
+const formatDateTimeString = (value) => {
+    if (!value) return value;
+    const date = value instanceof Date ? value : new Date(value);
+    if (isNaN(date)) return undefined;
+    const pad = (n) => String(n).padStart(2, '0');
+    const yyyy = date.getFullYear();
+    const mm = pad(date.getMonth() + 1);
+    const dd = pad(date.getDate());
+    const HH = pad(date.getHours());
+    const MM = pad(date.getMinutes());
+    const SS = pad(date.getSeconds());
+    return `${yyyy}-${mm}-${dd} ${HH}:${MM}:${SS}`;
+};
+
+const transform = (data) => ({
+    ...data,
+    updated_at: data?.updated_at ? formatDateTimeString(data.updated_at) : data?.updated_at,
+});
 
 const SettingVersionEdit = (props) => (
-    <Edit {...props}>
+    <Edit {...props} transform={transform}>
         <SimpleForm>
             <p>id 不能修改</p>
             <TextInput source="id" disabled alwaysOn /> {/* 設定屬性 alwaysOn 時 id 一定送出 */}
             <p>data_type 不能修改</p>
             <TextInput source="data_type" disabled />
             <TextInput source="version" />
-            <TextInput source="updated_at" /> {/* 一般不開放修改，但是版本號支援修改更新日期 */}
+            <DateTimeInput source="updated_at" options={{ ampm: false }} /> {/* 一般不開放修改，但是版本號支援修改更新日期 */}
         </SimpleForm>
     </Edit>
 );
