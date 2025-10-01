d743bf03688015549595ceb65d6d7ce347435597
chiisen <chiisen@hotmail.com>
2025-10-01 12:29:28 +0800

    ✨ feat: 新增語言篩選功能，包含重置按鈕以改善列表過濾體驗

--- my-admin/src/I18nText/I18nTextList.jsx
+++ my-admin/src/I18nText/I18nTextList.jsx
@@ -1,4 +1,5 @@
-import { CreateButton, TopToolbar } from 'react-admin';
+import { CreateButton, TopToolbar, Filter, SelectInput, Button } from 'react-admin';
+import { useListContext } from 'react-admin';
 
 
 import { List, DataTable } from 'react-admin';
@@ -20,13 +21,43 @@ const ListActions = ({ columns }) => (
 );
 
 
+const I18nTextFilter = () => {
+    const { setFilters } = useListContext();
+
+    const handleResetFilter = () => {
+        setFilters({});
+    };
+
+    return (
+        <Filter>
+            <SelectInput
+                source="lang"
+                label="語言"
+                alwaysOn
+                choices={[
+                    { id: 'de-DE', name: '德語 (de-DE)' },
+                    { id: 'en-US', name: '英文 (en-US)' },
+                    { id: 'es-ES', name: '西班牙語 (es-ES)' },
+                    { id: 'fr-FR', name: '法語 (fr-FR)' },
+                    { id: 'it-IT', name: '義大利語 (it-IT)' },
+                    { id: 'ja-JP', name: '日語 (ja-JP)' },
+                    { id: 'pt-PT', name: '葡萄牙語 (pt-PT)' },
+                    { id: 'zh-CN', name: '簡體中文 (zh-CN)' },
+                    { id: 'zh-TW', name: '繁體中文 (zh-TW)' },
+                ]}
+            />
+            <Button label="重置" onClick={handleResetFilter} />
+        </Filter>
+    );
+};
+
 const columns = [
-    <DataTable.Col key="id" source="id" align="right" />, 
-    <DataTable.Col key="key" source="key" align="left" />, 
-    <DataTable.Col key="lang" source="lang" align="left"  />, 
-    <DataTable.Col key="text" source="text" align="left"  />, 
-    <DataTable.Col key="updated_at" source="updated_at" align="left"  />, 
-    <DataTable.Col key="created_at" source="created_at" align="left"  />,
+    <DataTable.Col key="id" source="id" align="right" />,
+    <DataTable.Col key="key" source="key" align="left" />,
+    <DataTable.Col key="lang" source="lang" align="left" />,
+    <DataTable.Col key="text" source="text" align="left" />,
+    <DataTable.Col key="updated_at" source="updated_at" align="left" />,
+    <DataTable.Col key="created_at" source="created_at" align="left" />,
 ];
 
 
@@ -48,6 +79,7 @@ const I18nTextList = () => (
             title="多語系文字"
             actions={<ListActions columns={columns} />}
             pagination={<CustomPagination />}
+            filters={<I18nTextFilter />}
         >
             <DataTable className="center-header">
                 {columns}
